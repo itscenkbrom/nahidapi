2c2be4acf3df2ce4c3e05b6e1cbdca2dc3fe77a2
itscenkbrom <cenkethecanbutthe@gmail.com>
2025-10-18 18:57:41 +0400

    Update index.js

--- index.js
+++ index.js
@@ -10,137 +10,69 @@ app.use(express.json());
 // Fotoğraf ve artist verileri (bunu istediğin kadar artırabilirsin)
 const data = {
   results: [
-    {
-      artist_name: "たら/ tara",
-      url: "https://i.pximg.net/img-master/img/2025/10/18/18/18/24/136419989_p0_master1200.jpg"
-    },
-    {
-      artist_name: "アニメ猫",
-      url: "https://i.pximg.net/img-master/img/2025/10/12/14/42/00/136177763_p0_master1200.jpg"
-    },
-    {
-      artist_name: "lovetraveling",
-      url: "https://i.pximg.net/img-master/img/2025/10/17/21/58/13/136387535_p0_master1200.jpg"
-    },
-    {
-  artist_name: "变态萝莉控",
-  url: "https://i.pximg.net/img-master/img/2025/10/13/13/04/53/136219814_p0_master1200.jpg"
+   {
+  artist_name: "omgRimuu",
+  url: "https://i.redd.it/7mtv74ytsxh91.png"
 },
 {
-  artist_name: "わさび",
-  url: "https://i.pximg.net/img-master/img/2025/09/20/18/27/39/135325007_p0_master1200.jpg"
+  artist_name: "PolaiMath",
+  url: "https://i.redd.it/wfhpohzcsk5c1.jpg"
 },
 {
-  artist_name: "白百合",
-  url: "https://i.pximg.net/img-master/img/2025/09/23/00/14/32/135423417_p0_master1200.jpg"
+  artist_name: "Kamui",
+  url: "https://i.redd.it/qw527i3o0pn91.jpg"
 },
 {
-  artist_name: "眠",
-  url: "https://i.pximg.net/img-master/img/2025/09/16/18/00/01/135172603_p0_master1200.jpg"
+  artist_name: "Ryann",
+  url: "https://i.pinimg.com/736x/f2/44/4d/f2444dfc9e0c7c4e64597412b811bc4d.jpg"
 },
 {
-  artist_name: "めろは",
-  url: "https://i.pximg.net/img-master/img/2025/09/22/21/00/10/135414143_p0_master1200.jpg"
+  artist_name: "Orjinal çizim",
+  url: "https://i.pinimg.com/736x/d8/35/08/d83508121f9064ac90703b8d661b0369.jpg"
 },
 {
-  artist_name: "晚晚喵",
-  url: "https://i.pximg.net/img-master/img/2025/10/12/14/42/00/136177763_p0_master1200.jpg"
+  artist_name: "ごとー/gotoh",
+  url: "https://upload-os-bbs.hoyolab.com/upload/2023/04/18/235340161/68cd4f9e67483a10c24faceb332d5e00_6827247691906521503.jpg?x-oss-process=image%2Fresize%2Cs_1000%2Fauto-orient%2C0%2Finterlace%2C1%2Fformat%2Cwebp%2Fquality%2Cq_70"
 },
 {
-  artist_name: "しわ子",
-  url: "https://i.pximg.net/img-master/img/2025/09/23/03/10/08/135428420_p0_master1200.jpg"
+  artist_name: "ごとー/gotoh",
+  url: "https://upload-os-bbs.hoyolab.com/upload/2023/04/18/235340161/b49b99d8ecd7ec6f5ac01ed39da38ad3_4080935816671732147.jpg?x-oss-process=image%2Fresize%2Cs_1000%2Fauto-orient%2C0%2Finterlace%2C1%2Fformat%2Cwebp%2Fquality%2Cq_70"
 },
 {
-  artist_name: "Azu (秋の予感)",
-  url: "https://i.pximg.net/img-master/img/2025/09/23/11/13/39/135431716_p0_master1200.jpg"
+  artist_name: "透明Tomei",
+  url: "https://webstatic.hoyoverse.com/upload/event/2023/05/10/3a11160f0c89d07ab0c7211e704e7279_6850114456873958335.jpg?x-oss-process=image/format,webp/quality,Q_90"
 },
 {
-  artist_name: "胡ﾏ田 ﾏｺ",
-  url: "https://i.pximg.net/img-master/img/2025/09/23/01/24/56/135426026_p0_master1200.jpg"
+  artist_name: "@cmdxca",
+  url: "https://webstatic.hoyoverse.com/upload/event/2023/05/10/0e1f4f5dfc3bf3f937078d557f94f446_7469995819212079767.jpg?x-oss-process=image/format,webp/quality,Q_90"
 },
 {
-  artist_name: "めだ茶",
-  url: "https://i.pximg.net/img-master/img/2025/09/23/21/06/01/135455385_p0_master1200.jpg"
+  artist_name: "Yamyouw",
+  url: "https://webstatic.hoyoverse.com/upload/event/2023/05/10/aac7e1424f574462c8fd0349d51bd30e_2368863229368451857.jpg?x-oss-process=image/format,webp/quality,Q_90"
 },
 {
-  artist_name: "スバル",
-  url: "https://i.pximg.net/img-master/img/2025/09/28/07/58/13/135622747_p0_master1200.jpg"
+  artist_name: "HoNyo",
+  url: "https://webstatic.hoyoverse.com/upload/event/2023/05/10/2ebcc6f6f561d7b86d2770f32b499cfa_8949037441543876409.jpg?x-oss-process=image/format,webp/quality,Q_90"
 },
 {
-  artist_name: "LemonZTeA",
-  url: "https://i.pximg.net/img-master/img/2025/09/28/20/56/48/135647035_p0_master1200.jpg"
+  artist_name: "@mr1thonk",
+  url: "https://webstatic.hoyoverse.com/upload/event/2023/05/10/2452d2c2b565b920463e38454f5e1131_525517277231510511.jpg?x-oss-process=image/format,webp/quality,Q_90"
 },
 {
-  artist_name: "のんさん",
-  url: "https://i.pximg.net/img-master/img/2025/09/28/21/54/19/135649888_p0_master1200.jpg"
+  artist_name: "紺_",
+  url: "https://webstatic.hoyoverse.com/upload/event/2023/05/10/05790b07d281390729004eb938adaa67_7599353038525595451.jpg?x-oss-process=image/format,webp/quality,Q_90"
 },
 {
-  artist_name: "浅银黎星",
-  url: "https://i.pximg.net/img-master/img/2025/09/28/14/01/06/135632128_p0_master1200.jpg"
+  artist_name: "冰灕kori",
+  url: "https://webstatic.hoyoverse.com/upload/event/2023/05/10/b633230c2893ace86e0039ce92edfdc1_7781155569480902013.jpg?x-oss-process=image/format,webp/quality,Q_90"
 },
 {
-  artist_name: "Decopon",
-  url: "https://i.pximg.net/img-master/img/2025/09/29/23/06/15/135690585_p0_master1200.jpg"
+  artist_name: "AaaAaaAAaHyuN",
+  url: "https://webstatic.hoyoverse.com/upload/event/2023/05/10/e1e41af39dad90b40d571197269de5b5_6190656606068062762.jpg?x-oss-process=image/format,webp/quality,Q_90"
 },
 {
-  artist_name: "ころあい",
-  url: "https://i.pximg.net/img-master/img/2025/10/02/14/32/35/135784078_p0_master1200.jpg"
-},
-{
-  artist_name: "Neko(0w0)",
-  url: "https://i.pximg.net/img-master/img/2025/10/03/15/01/09/135820367_p0_master1200.jpg"
-},
-{
-  artist_name: "みラァ",
-  url: "https://i.pximg.net/img-master/img/2025/10/05/22/22/05/135921877_p0_master1200.jpg"
-},
-{
-  artist_name: "kumanee",
-  url: "https://i.pximg.net/img-master/img/2025/10/07/21/50/09/135995686_p0_master1200.jpg"
-},
-{
-  artist_name: "Black Swordsman",
-  url: "https://i.pximg.net/img-master/img/2025/10/08/07/15/32/136010926_p0_master1200.jpg"
-},
-{
-  artist_name: "Crob.フェロー",
-  url: "https://i.pximg.net/img-master/img/2025/09/19/00/40/51/135262353_p0_master1200.jpg"
-},
-{
-  artist_name: "Rio",
-  url: "https://i.pximg.net/img-master/img/2025/10/08/22/20/40/136029128_p0_master1200.jpg"
-},
-{
-  artist_name: "めとろ",
-  url: "https://i.pximg.net/img-master/img/2025/10/09/14/29/58/136056053_p0_master1200.jpg"
-},
-{
-  artist_name: "うちゆうじん",
-  url: "https://i.pximg.net/img-master/img/2025/10/12/01/18/41/136160011_p0_master1200.jpg"
-},
-{
-  artist_name: "kareyaカレー屋",
-  url: "https://i.pximg.net/img-master/img/2025/10/12/23/10/15/136198539_p0_master1200.jpg"
-},
-{
-  artist_name: "桃源莉可",
-  url: "https://i.pximg.net/img-master/img/2023/10/28/19/33/42/112920862_p0_master1200.jpg"
-},
-{
-  artist_name: "lovetraveling",
-  url: "https://i.pximg.net/img-master/img/2025/09/18/04/15/17/135230894_p0_master1200.jpg"
-},
-{
-  artist_name: "稲羽の青兎",
-  url: "https://i.pximg.net/img-master/img/2025/10/16/00/48/32/136322812_p0_master1200.jpg"
-},
-{
-  artist_name: "踏梦_Official",
-  url: "https://i.pximg.net/img-master/img/2025/10/12/12/32/46/136174164_p0_master1200.jpg"
-},
-{
-  artist_name: "霜月ミナト",
-  url: "https://i.pximg.net/img-master/img/2025/10/18/18/18/24/136419989_p0_master1200.jpg"
+  artist_name: "玄米Kome",
+  url: "https://webstatic.hoyoverse.com/upload/event/2023/05/10/3e97f51da660125f0572e187657a67d5_3049130503459032752.jpg?x-oss-process=image/format,webp/quality,Q_90"
 }
   ]
 };
@@ -170,4 +102,5 @@ app.get('/health', (req, res) => {
 app.listen(port, () => {
   console.log(`NahidaApi listening at http://localhost:${port}`);
 });
+
 
